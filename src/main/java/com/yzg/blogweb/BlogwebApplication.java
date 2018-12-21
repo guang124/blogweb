10fc1230614253afe0b0e1c0f0f0314aeff63b08
guang124 <1758090306@qq.com>
2018-12-21 14:46:46 +0800

    Spring boot +mybatis整合及helloWord

---
+++ src/main/java/com/yzg/blogweb/BlogwebApplication.java
@@ -0,0 +1,14 @@
+package com.yzg.blogweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogwebApplication.class, args);
+    }
+
+}
+
